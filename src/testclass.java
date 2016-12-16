c57e4bc0e5b25c8566a5aaae90c9d5d0943cf175
Dante <dante_cancer27@hotmail.com>
2016-12-16 21:31:47 +0100

    holas

--- src/testclass.java
+++ src/testclass.java
@@ -1,4 +1,27 @@
 
 public class testclass {
-
+	
+	public static void main(String[] args) {
+		
+		int l = args.length;
+		int i = 0;
+		
+		
+		for (i=0; i < l;i++){
+			System.out.println("palabra  "+i +" = "+args[i]);
+			
+		}
+		
+		
+		
+		int nPalabras = args.length;
+		System.out.println("Son "+nPalabras+" palabras");
+	}
+	
+	
+	
+	
+	
+	
+	
 }
