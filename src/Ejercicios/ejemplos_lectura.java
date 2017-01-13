a2e19f28bf518ed7f54b7d0c0eace3d800e638a6
Dante <dante_cancer27@hotmail.com>
2017-01-13 19:35:30 +0100

    ya

--- src/Ejercicios/ejemplos_lectura.java
+++ src/Ejercicios/ejemplos_lectura.java
@@ -1,28 +1,29 @@
 package Ejercicios;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
 public class ejemplos_lectura {
 
 	public static void leer_fichero(String fichero) throws IOException{
-		
+		String dentroFichero="";
 		FileReader fichero1 = new FileReader(new File(fichero));
 		int i=0;
 		int charAux;
 		int suma=0;
 		
 		
-		while(i!=-1){
-			charAux=fichero1.read();
+		while((i=fichero1.read())!=-1){
 			
-			i=charAux;
+			charAux=i;
+			
+			
 			char aux = (char)charAux;
 			
+			dentroFichero = dentroFichero + aux;
 			suma = suma + i;
-		System.out.println("La letra es "+aux);
+		System.out.println("Dentro del fichero hay "+dentroFichero);
 		}
 	
 		System.out.println("La suma de la equivalencia int de todos los caracteres es "+suma);
