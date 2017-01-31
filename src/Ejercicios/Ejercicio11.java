8b3ec19f98eec4f42fede599e17ac5ff22dbd6b0
Dante <dante_cancer27@hotmail.com>
2017-01-31 18:37:00 +0100

    Lista de Libros

--- src/Ejercicios/Ejercicio11.java
+++ src/Ejercicios/Ejercicio11.java
@@ -1,7 +1,13 @@
 package Ejercicios;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Properties;
+import java.util.Scanner;
 
 public class Ejercicio11 {
 
@@ -9,28 +15,69 @@ public class Ejercicio11 {
 	 * Leer las historias segun el argumento que te ponga el usuario
 	 * y volcarlo a un nuevo fichero. por switch + cases y  transformar el
 	 * Start, End, Body.
+	 * @throws FileNotFoundException 
 	 */
 	
-	public static void main(String[] args) {
-		String historia = null;
-		Properties fichero = new Properties();
-		FileReader fR = new FileReader("story_es.properties");
-		String aux="es";
-		switch(args[0]){
-		
-		
-		case args[0]:
-			
-			
-			
-			
-		}
-		
-		
-		
-		}
-		
-		
-	}
+//	public static String recibirPalabra(){
+//	Scanner sc = new Scanner(System.in);
+//	
+//	String aux = sc.nextLine();
+//	
+//	return aux;
+//	}
 	
-}
+	private static final String PREF = "story_";
+	private static final String SUF = ".properties";
+			
+	public static void main(String[] args) throws Exception {
+		String nF= args[0];
+		Properties fichero = new Properties();
+		String dirFich= PREF+nF+SUF;
+		//System.out.println(dirFich);
+		
+		FileReader fR = new FileReader(dirFich);
+		
+		try {
+			fichero.load(fR);
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.out.println("error abriendo");
+		}
+		//File fichero2 = new File(dirFich+"1");
+				//fichero2.createNewFile();
+		//FileWriter escrF = new FileWriter(fichero2);
+				//BufferedWriter escrF2 = new BufferedWriter(escrF2);
+		
+		//System.out.println("En que formato de idioma quiere la historia?\n -es\n -en \n -it");
+		String aux="";
+		switch(args[0]){
+			
+		
+			
+		case "es":
+			aux =fichero.getProperty("outfile")+": "+ fichero.getProperty("start")+" "+fichero.getProperty("body")+" "+fichero.getProperty("end");
+		System.out.println(aux);
+		
+		//escrF.write(aux);
+		//escrF.close();
+		case "en":
+		aux =fichero.getProperty("outfile")+": "+ fichero.getProperty("start")+" "+fichero.getProperty("body")+" "+fichero.getProperty("end");
+		System.out.println(aux);
+		
+		//escrF.write(aux);
+		//escrF.close();
+		case "it":
+
+		aux =fichero.getProperty("outfile")+": "+ fichero.getProperty("start")+" "+fichero.getProperty("body")+" "+fichero.getProperty("end");
+			System.out.println(aux);
+		
+			//escrF.write(aux);
+			//escrF.close();
+		}
+		
+		fR.close();
+		
+		}
+}	
+	
+
---
+++ src/minipracts/LibroXML.java
@@ -0,0 +1,69 @@
+package minipracts;
+
+import java.util.ArrayList;
+
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+
+public class LibroXML extends DefaultHandler{
+	 private String valor = null;  
+     
+	   private Libro libro;  
+	     
+	   public LibroXML(Libro libro){  
+	      this.libro=libro;  
+	   } 
+	   
+	   private ArrayList<Libro> listaLibros = new ArrayList<>();
+		
+
+	
+	     
+	   @Override  
+	   public void startElement(String uri, String localName, String name,  
+	         Attributes attributes) throws SAXException {  
+	        
+	      // Limpiamos la variable temporal.  
+	      valor=null;  
+	        
+	      // Si la etiqueta es libro leemos el atributo isbn  
+	      if(localName.equals("libro")){  
+	         String isbn = attributes.getValue("isbn");  
+	         // Lo guardamos en el objeto libro  
+	         libro.setIsbn(isbn);  
+	      }  
+	   }  
+	     
+	   @Override  
+	   public void characters(char[] ch, int start, int length)  
+	         throws SAXException {  
+	      // Guardamos el texto en la variable temporal  
+	      valor = new String(ch,start,length);  
+	      System.out.println(valor);
+	   }  
+	  
+	   @Override  
+	   public void endElement(String uri, String localName, String name)  
+	         throws SAXException {  
+	      // Según la etiqueta guardamos el valor leido   
+	      // en una propiedad del objeto libro  
+		   System.out.println(name);
+	      if (localName.equals("titulo")){  
+	         libro.setTitulo(valor);  
+	      }else if (localName.equals("autor")){  
+	         libro.setAutor(valor);  
+	      }else if (localName.equals("anyo")){  
+	         libro.setAnyo(valor);  
+	      }else if (localName.equals("editorial")){  
+	         libro.setEditorial(valor);  
+	      }  
+	        
+	      listaLibros.add(this.libro);
+	      //listaLibros.clear();
+	      
+	      //System.out.println(listaLibros.get(0));
+	      
+	   }  
+	  
+	}  
